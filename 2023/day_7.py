89bc7c12bc003163f0b8b1b450f30ae5c907179d
Vincent Delbar <vincent.delbar@latelescop.fr>
2023-12-11 00:06:29 +0100

    Add day 7 part 2

--- 2023/day_7.py
+++ 2023/day_7.py
@@ -3,43 +3,86 @@ from collections import Counter
 CARDS = list("AKQJT98765432")
 
 
-def hand_type(hand: Counter) -> int:
-    mx, le = max(hand.values()), len(hand.values())
-    if mx == 5:
-        return 0  # five of a kind
-    if mx == 4:
-        return 1  # four of a kind
-    if mx == 3 and le == 2:
-        return 2  # full house
-    if mx == 3:
-        return 3  # thee of a kind
-    if mx == 2 and le == 3:
-        return 4  # two pair
-    if mx == 2:
-        return 5  # one pair
-    return 6  # high_card
-
-
 def cards_score(hand: str):
     return tuple(CARDS.index(c) for c in hand)
 
 
-def parse_games(text: str):
-    hands = []
-    for game in text.splitlines():
-        hand, bid = game.split()
-        counter = Counter(hand)
-        hands.append((hand, int(bid), hand_type(counter), cards_score(hand)))
-    return hands
+def hand_type(hand: Counter) -> int:
+    mx, le = max(hand.values()), len(hand.values())
+    if mx == 5:
+        htype = 0  # five of a kind
+    elif mx == 4:
+        htype = 1  # four of a kind
+    elif mx == 3 and le == 2:
+        htype = 2  # full house
+    elif mx == 3:
+        htype = 3  # thee of a kind
+    elif mx == 2 and le == 3:
+        htype = 4  # two pair
+    elif mx == 2:
+        htype = 5  # one pair
+    else:
+        htype = 6  # high_card
+    return htype
 
 
 def part_1(text: str):
-    games = sorted(parse_games(text), key=lambda g: (g[2], g[3]), reverse=True)
+    games = []
+    for game in text.splitlines():
+        hand, bid = game.split()
+        counter = Counter(hand)
+        games.append((hand, int(bid), hand_type(counter), cards_score(hand)))
+
+    games = sorted(games, key=lambda g: (g[2], g[3]), reverse=True)
     return sum(i * g[1] for i, g in enumerate(games, 1))
 
 
+def joker_hand_type(hand: Counter) -> int:
+    mx, le = max(hand.values()), len(hand.values())
+    jk = hand.get("J") or 0
+    if (
+        mx == 5
+        or jk == 4
+        or (mx == 4 and jk == 1)
+        or (mx == 3 and jk == 2)
+        or (jk == 3 and le == 2)
+    ):
+        htype = 0  # five of a kind
+    elif mx == 4 or (mx == 3 and jk == 1) or (jk >= 2 and le == 3):
+        htype = 1  # four of a kind
+    elif mx == 3 and le == 2 or jk == 1 and le == 3:
+        htype = 2  # full house
+    elif mx == 3 or jk == 2 or mx == 2 and jk == 1:
+        htype = 3  # thee of a kind
+    elif mx == 2 and le == 3:
+        htype = 4  # two pair
+    elif mx == 2 or mx == 1 and jk == 1:
+        htype = 5  # one pair
+    else:
+        htype = 6  # high_card
+    return htype
+
+
+CARDS_WITH_JOKER = list("AKQT98765432J")
+
+
+def joker_cards_score(hand: str):
+    return tuple(CARDS_WITH_JOKER.index(c) for c in hand)
+
+
 def part_2(text: str):
-    ...
+    games = []
+    for game in text.splitlines():
+        hand, bid = game.split()
+        counter = Counter(hand)
+        htype = joker_hand_type(counter)
+        score = joker_cards_score(hand)
+        games.append((hand, int(bid), htype, score))
+
+    games = sorted(games, key=lambda g: (g[2], g[3]), reverse=True)
+    for i, g in enumerate(games, 1):
+        print(i, g)
+    return sum(i * g[1] for i, g in enumerate(games, 1))
 
 
 if __name__ == "__main__":
