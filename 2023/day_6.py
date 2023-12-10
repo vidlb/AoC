58d9e7b9a58917f0ab6b04dec4fcb59574216dc1
Vincent Delbar <vincent.delbar@latelescop.fr>
2023-12-10 18:23:03 +0100

    Simplify day 6

--- 2023/day_6.py
+++ 2023/day_6.py
@@ -1,37 +1,22 @@
 from math import prod
 
 
-def parse_races(text: str) -> tuple[tuple[int]]:
-    time, dist, _ = text.split("\n")
-    time, dist = time.split(":")[1].split(), dist.split(":")[1].split()
-    return tuple(int(t) for t in time), tuple(int(d) for d in dist)
-
-
-def button_times(total_time, dist) -> tuple[int]:
-    times = []
-    for i in range(total_time):
-        run_time = total_time - i
-        if i * run_time > dist:
-            times.append(i)
-    return tuple(times)
+def n_times(time, dist) -> tuple[int]:
+    return tuple(i for i in range(time) if i * (time - i) > dist)
 
 
 def part_1(text: str) -> int:
-    time, dist = parse_races(text)
-    runs = []
-    for t, d in zip(time, dist):
-        runs.append(len(button_times(t, d)))
-    return prod(runs)
-
-
-def parse_single_race(text: str) -> tuple[int]:
     time, dist, _ = text.split("\n")
-    time, dist = time.split(":")[1].replace(" ", ""), dist.split(":")[1].replace(" ", "")
-    return int(time), int(dist)
+    time = tuple(int(t) for t in time.split(":")[1].split())
+    dist = tuple(int(d) for d in dist.split(":")[1].split())
+    return prod(len(n_times(t, d)) for t, d in zip(time, dist))
 
 
 def part_2(text: str) -> int:
-    return len(button_times(*parse_single_race(text)))
+    time, dist, _ = text.split("\n")
+    time = time.split(":")[1].replace(" ", "")
+    dist = dist.split(":")[1].replace(" ", "")
+    return len(n_times(int(time), int(dist)))
 
 
 if __name__ == "__main__":
