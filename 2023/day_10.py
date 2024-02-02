0bc9dca55c5fb2496f35b71f5a8d44fa5ac79ae7
Vincent Delbar <vincent.delbar@latelescop.fr>
2024-02-02 14:58:27 +0100

    Add day 10 part 2 (thanks to reddit)

--- 2023/day_10.py
+++ 2023/day_10.py
@@ -1,31 +1,25 @@
-# North, east, south, west
+# Clockwise : north, east, south, west
 POS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-INVERT_POS = {0: 2, 1: 3, 2: 0, 3: 1, -1: -1}
-LUT = {
-    "|": [("7", "F", "|"), (), ("J", "L", "|"), ()],
+INV_POS = {0: 2, 1: 3, 2: 0, 3: 1, -1: -1}
+CONNECTIBLE_PIPES = {
     "-": [(), ("7", "J", "-"), (), ("L", "F", "-")],
-    "L": [("7", "F", "|"), ("7", "J", "-"), (), ()],
-    "J": [("7", "F", "|"), (), (), ("F", "L", "-")],
+    "|": [("7", "F", "|"), (), ("J", "L", "|"), ()],
     "7": [(), (), ("L", "J", "|"), ("F", "L", "-")],
+    "J": [("7", "F", "|"), (), (), ("F", "L", "-")],
     "F": [(), ("7", "J", "-"), ("L", "J", "|"), ()],
+    "L": [("7", "F", "|"), ("7", "J", "-"), (), ()],
 }
-
-
-def find_start(data: list):
-    for n, line in enumerate(data):
-        if "S" in line:
-            return n, line.index("S")
 
 
 def guess_symbol(data: list, y: int, x: int):
     count = {}
-    for symbol in LUT:
+    for symbol in CONNECTIBLE_PIPES:
         pipes = 0
         for i, pos in enumerate(POS):
             next_y, next_x = y + pos[0], x + pos[1]
             if not 0 <= next_y < len(data) or not 0 <= next_x < len(data[next_y]):
                 continue
-            if data[next_y][next_x] in LUT[symbol][i]:
+            if data[next_y][next_x] in CONNECTIBLE_PIPES[symbol][i]:
                 pipes += 1
         count[symbol] = pipes
     return max(count, key=count.get)
@@ -39,29 +33,48 @@ def next_pipe_idx(data: list, orig: int, char: str, y: int, x: int) -> tuple[int
         if not 0 <= next_y < len(data) or not 0 <= next_x < len(data[next_y]):
             continue
         next_char = data[next_y][next_x]
-        if next_char in LUT[char][i]:
-            return INVERT_POS[i], next_y, next_x
+        if next_char in CONNECTIBLE_PIPES[char][i]:
+            return i, next_y, next_x
     return -1, -1, -1  # found "S": loop is complete
 
 
-def part_1(data: list) -> int:
+def find_start(data: list):
+    for n, line in enumerate(data):
+        if "S" in line:
+            return n, line.index("S")
+
+
+def pipe_coords(data: list) -> list[tuple]:
     y, x = find_start(data)
     char = guess_symbol(data, y, x)
-    origin = -1
-    i = 0
+    coords = [(y, x)]
+    direction = -1
     while y >= 0 and x >= 0:
-        origin, y, x = next_pipe_idx(data, origin, char, y, x)
+        direction, y, x = next_pipe_idx(data, INV_POS[direction], char, y, x)
         char = data[y][x]
-        i += 1
-    return i // 2
+        if x >= 0 and y >= 0:
+            coords.append((y, x))
+    return coords
 
 
-def part_2() -> int:
-    return 0
+def part_1(data: list) -> int:
+    return len(pipe_coords(data)) // 2
+
+
+def part_2(data: list) -> int:
+    coords = pipe_coords(data)
+    padded_coords = [*coords, coords[0]]
+    pairs = zip(padded_coords, padded_coords[1:])
+    # Shoelace area
+    area = (
+        abs(sum(row1 * col2 - row2 * col1 for (row1, col1), (row2, col2) in pairs)) / 2
+    )
+    # Pick's theorem
+    return int(area - 0.5 * len(coords) + 1)
 
 
 if __name__ == "__main__":
     with open("data_10.txt", encoding="utf-8") as io:
         data = io.read().splitlines()
     print(f"Part 1 answer is {part_1(data)}")
-    # print(f"Part 2 answer is {part_2(data)}")
+    print(f"Part 2 answer is {part_2(data)}")
